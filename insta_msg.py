fe2f973b66c8f80c6868e8dcd6935a189196346a
Aditya Sahoo <adidesign264@gmail.com>
2024-05-16 09:39:17 +0530

    Update insta_msg.py

--- insta_msg.py
+++ insta_msg.py
@@ -7,23 +7,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 
-# Initializing Chrome WebDriver
+# Initialize Chrome WebDriver
 driver = webdriver.Chrome()
-
-# Maximizing Chrome Window
 driver.maximize_window()
+
 driver.implicitly_wait(10)
 
-# Navigating to the Instagram login page
+# Navigatinh to the Instagram login page
 driver.get("https://www.instagram.com/accounts/login/")
 sleep(2)
 
+# Entering the Details(username & password)
 username_input = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-username_input.send_keys("avengersunited911@gmail.com")
+username_input.send_keys("asahoo264200@gmail.com")
 sleep(2)
 
+# Finding the user
 password_input = driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[2]/div/label/input')
-password_input.send_keys("VkZLxXaX_BzCu8_")
+password_input.send_keys("test_account")
 sleep(2)
 
 
@@ -41,19 +42,21 @@ search = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div
 
 sleep(2)
 search_user = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/input')
-search_user.send_keys("saurabh prime")
+search_user.send_keys("unk264_os")
 
 sleep(2)
 user_found = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/a[1]/div[1]/div/div/div[1]/div/div/object/a')
 user_found.click()
 
 sleep(2)
-user_msg = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[1]/div/div[2]/div')
+user_msg = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div')
 user_msg.click()
 
+# Sending the automated message
 sleep(6)
 msg = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[1]/p')
-msg.send_keys("Message automation using Selenium")
+msg.send_keys("Automated Message using Selenium")
 sleep(3)
 msg.send_keys(Keys.ENTER)
-sleep(200)
+sleep(10)
+print("Automation closed")
